97a1ff275fa45a86120032995f7afd63f2f94724
Aleksandr <aleksandr_ermakov@outlook.com>
2026-03-23 16:43:06 +0300

    fist part without good styles done

--- src/components/FormRegistration/FormRegistration.jsx
+++ src/components/FormRegistration/FormRegistration.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import styles from "./FormRegistration.module.scss";
 
 function FormRegistration() {
@@ -6,31 +6,86 @@ function FormRegistration() {
   const [emailError, setEmailError] = useState("");
   const [password, setPassword] = useState("");
   const [passwordError, setPasswordError] = useState("");
-  const [repeatPassword, setrepeatPassword] = useState("");
-  const [repeatPasswordError, setrepeatPasswordError] = useState("");
+  const [repeatPassword, setRepeatPassword] = useState("");
+  const [repeatPasswordError, setRepeatPasswordError] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [showRepetPassword, setShowRepeatPassword] = useState(false);
+  const submitButtonRef = useRef(null);
 
   const emailRegex =
     /^[a-zA-Z0-9]([a-zA-Z0-9._-])*[a-zA-Z0-9]@[a-zA-Z0-9]([a-zA-Z0-9-])*[a-zA-Z0-9]\.[a-zA-Z]{2,}$/;
 
-  // /^[a-zA-Z0-9]([a-zA-Z0-9._-])*[a-zA-Z0-9]@[a-zA-Z0-9]([a-zA-Z0-9-])*[a-zA-Z0-9]\.[a-zA-Z]{2,}$/;
+  const passwordRegex =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[#@#$%^&*])[a-zA-Z\d#@#$%^&*]{8,128}$/;
 
-  // const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[#@#$%^&*])[a-zA-Z\d#@#$%^&*]{8,128}$/;
+  const checkSubmitFocus = (data) => {
+    const newData = {
+      email,
+      password,
+      repeatPassword,
+      emailError,
+      passwordError,
+      repeatPasswordError,
+      ...data,
+    };
+    if (
+      !newData.emailError &&
+      !newData.passwordError &&
+      !newData.repeatPasswordError &&
+      newData.email &&
+      newData.password &&
+      newData.repeatPassword
+    ) {
+      setTimeout(() => {
+        submitButtonRef.current?.focus();
+      }, 0);
+    }
+  };
 
   const onEmailChange = ({ target }) => {
+    const value = target.value;
     let error = null;
-    setEmail(target.value);
-    if (!emailRegex.test(target.value)) {
+    if (!emailRegex.test(value)) {
       error = "email invalid";
     }
-    console.log("error", error);
+    setEmail(value);
     setEmailError(error);
+    checkSubmitFocus({
+      email: value,
+      emailError: error,
+    });
   };
-  const onPasswordChange = () => {};
-  const onRepeatPasswordChange = () => {};
 
+  const onPasswordChange = ({ target }) => {
+    const value = target.value;
+    let error = null;
+    if (!passwordRegex.test(value)) {
+      error = "invalid password";
+    }
+    setPassword(target.value);
+    setPasswordError(error);
+    checkSubmitFocus({
+      password: value,
+      passwordError: error,
+    });
+  };
+
+  const onRepeatPasswordChange = ({ target }) => {
+    const value = target.value;
+    let error = null;
+    if (target.value !== password) {
+      error = "the passwords must match";
+    }
+    setRepeatPassword(target.value);
+    setRepeatPasswordError(error);
+    checkSubmitFocus({
+      repeatPassword: value,
+      repeatPasswordError: error,
+    });
+  };
   const onSubmit = (e) => {
     e.preventDefault();
-    console.log(email);
+    console.log(email, password);
   };
 
   return (
@@ -39,6 +94,7 @@ function FormRegistration() {
         <form className={styles.FormRegistration} onSubmit={onSubmit}>
           <h1>Alena</h1>
           {emailError && <div className={styles.errorLabel}>{emailError}</div>}
+
           <input
             type="email"
             name="email"
@@ -46,28 +102,41 @@ function FormRegistration() {
             placeholder="email"
             onChange={onEmailChange}
           />
+
           {passwordError && (
             <div className={styles.errorLabel}>{passwordError}</div>
           )}
+
           <input
-            type="password"
-            name=""
+            // type="password"
+            type="text"
+            name="password"
             value={password}
             placeholder="password"
             onChange={onPasswordChange}
           />
+
           {repeatPasswordError && (
             <div className={styles.errorLabel}>{repeatPasswordError}</div>
           )}
           <input
-            type="password"
+            // type="password"
+            type="text"
             name="repeatPassword"
             value={repeatPassword}
             placeholder="repeat password"
             onChange={onRepeatPasswordChange}
           />
           <button
-            disabled={!!emailError || !!passwordError || !!repeatPasswordError}
+            ref={submitButtonRef}
+            disabled={
+              !!emailError ||
+              !!passwordError ||
+              !!repeatPasswordError ||
+              !email ||
+              !password ||
+              !repeatPassword
+            }
           >
             send
           </button>
